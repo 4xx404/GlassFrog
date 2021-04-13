6757971fd893ef893fed7dfca465e02d2adcc385
4xx404 <45526353+4xx404@users.noreply.github.com>
2021-04-13 02:37:19 +0100

    Update glassFrog.php

--- GlassFrog-UI/glassFrog.php
+++ GlassFrog-UI/glassFrog.php
@@ -2,7 +2,7 @@
 <head>
 	<title>GlassFrog UI</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="icon" type="image/png" href="assets/favicon.png"/>
+	<link rel="icon" type="image/png" href="favicon.png"/>
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<script src="js/glassFrog.js"></script>
 </head>
