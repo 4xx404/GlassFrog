7ba2befc51d026b6b4d8195ca4b3108960243a7e
4xx404 <45526353+4xx404@users.noreply.github.com>
2022-01-18 21:47:47 +0000

    Create Header.php

---
+++ GlassFrog-UI/Includes/Generic/Header.php
@@ -0,0 +1,5 @@
+<title><?= $Config->Get("app_data/name"); ?></title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="icon" type="image/png" href="<?= $Config->Get('app_data/favicon'); ?>"/>
+<link rel="stylesheet" type="text/css" href="Css/Generic.css">
+<script src="JavaScript/GlassFrog.js"></script>
