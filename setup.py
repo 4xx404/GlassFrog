e7f56b29bce1cca5dcb810ed774ef1af27084172
4xx404 <45526353+4xx404@users.noreply.github.com>
2022-01-18 23:21:14 +0000

    Added Error Handling

--- setup.py
+++ setup.py
@@ -8,37 +8,20 @@ from Core.Styling.Colors import bc
 from Core.Config import CoreConfig
 from Core.Commands import Command
 from Core.Database import DBManager
+from Core.Error import ErrorHandler
 
 class SetupGlassFrog:
 	def __init__(self):
 		self.Config = CoreConfig()
 		self.Cmd = Command()
+		self.Error = ErrorHandler()
 
-	def ThrowError(self, ErrorType: str, ErrorData: str or list = None):
-		self.ErrorType: str = ErrorType.lower()
-		self.ErrorData: str = ErrorData
-
-		self.DefinedErrors = [
-			"dependency_install_failed",
-			"ui_file_move_failed"
-		]
-
-		if(self.ErrorType in self.DefinedErrors):
-			if(self.ErrorType == "dependency_install_failed"):
-				return f"{sd.eBan} Failed to install dependencies from {bc.RC}{self.ErrorData}{bc.BC}\n\n{sd.eBan} Setup failed"
-			elif(self.ErrorType == "ui_file_move_failed"):
-				return f"{sd.eBan} Failed to move {bc.RC}{self.ErrorData[0]}/{bc.BC} to {bc.RC}{self.ErrorData[1]}{bc.BC}\n\n{sd.eBan} Setup failed"
-			else:
-				return f"{sd.eBan} Error Type {bc.RC}{self.ErrorType}{bc.BC} thrown without an error message defined"
-		else:
-			return f"{sd.eBan} Undefined Error Type {bc.RC}{self.ErrorType}{bc.BC}"
-	
 	def DependencyInstall(self):
 		print(f"{bc.BC} Running GlassFrog setup...\n Installing Dependencies...\n")
 		if(self.Cmd.InstallPythonDependencies()):
 			print(f"{sd.sBan} Dependencies Installed")
 		else:
-			self.Cmd.Quit(self.ThrowError("dependency_install_failed", self.Config.RequirementsFilePath))
+			self.Cmd.Quit(self.Error.Throw("dependency_install_failed", self.Config.RequirementsFilePath))
 	
 	def MoveUIFiles(self):
 		MovedOK = self.Cmd.MoveFile(self.Config.UIFilePath, self.Config.WebServerPath)
@@ -47,7 +30,7 @@ class SetupGlassFrog:
 		elif(MovedOK == None):
 			print(f"\n{sd.iBan.replace(' ERROR:', '')} {bc.GC}{self.Config.UIFilePath}/{bc.BC} has already been moved to {bc.GC}{self.Config.WebServerPath}{bc.BC}\n")
 		else:
-			self.Cmd.Quit(self.ThrowError("ui_file_move_failed", [self.Config.UIFilePath, self.Config.WebServerPath]))
+			self.Cmd.Quit(self.Error.Throw("ui_file_move_failed", [self.Config.UIFilePath, self.Config.WebServerPath]))
 	
 	def CreateDatabaseTables(self):
 		self.Database = DBManager()
