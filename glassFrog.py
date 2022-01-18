8b85263f57d1ea5d3e8cc9ee7b8e7e455a6a0f33
4xx404 <45526353+4xx404@users.noreply.github.com>
2022-01-18 21:31:58 +0000

    Update glassFrog.py

--- glassFrog.py
+++ glassFrog.py
@@ -1,109 +1,117 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
+import sys
+sys.dont_write_bytecode = True
 
-import sys, os, time
-import requests
-import sqlite3
-from bs4 import BeautifulSoup
-from modules.connectDB import database
-from modules.collectBranches import collector
+from Core.Styling.Banners import sd
+from Core.Styling.Colors import bc
 
-class bc:
-	GC = '\033[1;39m'
-	BC = '\033[1;34m'
-	RC = '\033[1;31m'
+from Core.Config import CoreConfig
+from Core.Commands import Command
+from Core.Database import DBManager
+from Core.Validity import Validation
+from Core.Collector import CollectionManager
+from Core.Scanner import ContentScanner
+from server import GlassFrogServer
 
-iBan = bc.BC + " [" + bc.GC + "?" + bc.BC + "]"
-sBan = bc.BC + " [" + bc.GC + u'\u2713' + bc.BC + "]"
-eBan = bc.BC + " [" + bc.RC + u'\u2717' + bc.BC + "]"
+class GlassFrog:
+	def __init__(self):
+		self.Config = CoreConfig()
+		self.Cmd = Command()
+		self.Database = DBManager()
+		self.Validator = Validation()
+		self.Collector = CollectionManager()
 
-author = bc.BC + "\n Author: " + bc.RC + "4" + bc.GC + "x" + bc.BC + "x" + bc.RC + "4" + bc.GC + "0" + bc.BC + "4\n"
-version = bc.BC + " Version: " + bc.RC + "2" + bc.GC + "." + bc.BC + "0\n"
-github = bc.BC + " Github: " + bc.RC + "h" + bc.GC + "t" + bc.BC + "t" + bc.RC + "p" + bc.GC + "s" + bc.BC + ":" + bc.RC + "/" + bc.GC + "/" + bc.BC + "g" + bc.RC + "i" + bc.GC + "t" + bc.BC + "h" + bc.RC + "u" + bc.GC + "b" + bc.BC + "." + bc.RC + "c" + bc.GC + "o" + bc.BC + "m" + bc.RC + "/" + bc.GC + "4" + bc.BC + "x" + bc.RC + "x" + bc.GC + "4" + bc.BC + "0" + bc.RC + "4\n"
+	def PrintHelpMenu(self):
+		print(f"{sd.iBan} Include {bc.GC}http://{bc.BC} or {bc.GC}https://{bc.BC} in URL")
+		print(f"\t{sd.eBan} {bc.RC}example.com{bc.BC}")
+		print(f"\t{sd.eBan} {bc.RC}www.example.com{bc.BC}")
+		print(f"\t{sd.sBan} {bc.GC}https://example.com{bc.BC}")
+		print(f"\t{sd.sBan} {bc.GC}https://www.example.com\n{bc.BC}")
 
-banner = bc.RC + '''
-''' + bc.GC + '''   ___  __      __    ___  ___  ____  ____  _____  ___     _   _   
-''' + bc.BC + '''  / __)(  )    /__\  / __)/ __)( ___)(  _ \(  _  )/ __)   (.)_(.)  
-''' + bc.RC + ''' ( (_-. )(__  /(__)\ \__ \\\__ \ )__)  )   / )(_)(( (_-.  (   _   ) 
-''' + bc.GC + '''  \___/(____)(__)(__)(___/(___/(__)  (_)\_)(_____)\___/  /`-----'\ 
-''' + author + version + github
-
-os.system('clear')
-print(banner)
-
-db = database()
-db.createTables()
-
-print('\n' + iBan + ' Include ' + bc.GC + 'http://' + bc.BC + ' or ' + bc.GC + 'https://' + bc.BC + ' in URL')
-print('\t' + eBan + bc.RC + ' example.com')
-print('\t' + eBan + bc.RC + ' www.example.com')
-print('\t' + sBan + bc.GC + ' https://example.com')
-print('\t' + sBan + bc.GC + ' https://www.example.com\n')
-
-def glassFrog():
-	base_url = ''
-	keyword = ''
-	try:
-		base_url = str(input(bc.BC + ' Base URL: ' + bc.GC))
-		keyword = str(input(bc.BC + ' Keyword: ' + bc.GC))
-		if(base_url == ''):
-			os.system('clear')
-			print(banner)
-			print(eBan + bc.RC + ' ERROR: ' + bc.BC + 'Base URL value cannot be empty\n')
-			time.sleep(1)
-			glassFrog()
-		elif(keyword == ''):
-			os.system('clear')
-			print(banner)
-			print(eBan + bc.RC + ' ERROR: ' + bc.BC + 'Keyword value cannot be empty\n')
-			time.sleep(1)
-			glassFrog()
-		else:
-			keyword = ' ' + str(keyword) + ' '
-			if(base_url.endswith('/')):
-				base_url = base_url
+	def SetBaseURL(self):
+		self.PrintHelpMenu()
+		self.BaseURL = str(input(f"{bc.BC} Base URL: {bc.GC}"))
+		if(self.Validator.NotEmpty(self.BaseURL)):
+			if(self.BaseURL.endswith("/")):
+				self.BaseURL = self.BaseURL
 			else:
-				base_url = base_url + '/'
-
-			if(base_url.startswith('http://') or base_url.startswith('https://')):
-				setBranches = ['' , '', '']
-				branch = collector()
-				branch.addBranches(base_url, keyword)
-			else:
-				os.system('clear')
-				print(banner)
-				print(eBan + bc.RC + ' ERROR: ' + bc.BC + 'Base URL must start with ' + bc.RC + 'http://' + bc.BC + ' or ' + bc.RC + 'https://' + '\n')
-				time.sleep(1)
-				glassFrog()
-
-			try:
-				os.system('sort data-analysis/UNKNOWN-TYPE-DATA.txt | uniq -d >> data-analysis/UNKNOWN-TYPE-DATA.txt')
-			except Exception:
-				print('\n' + eBan + ' Failed to remove duplicates from: ' + bc.RC + 'data-analysis/UNKNOWN-TYPE-DATA.txt\n')
-				input(bc.BC + ' Press Enter to Continue...')
-
-			print('\n' + sBan + ' Removed duplicates from ' + bc.GC + 'data-analysis/UNKNOWN-TYPE-DATA.txt\n')
-
-			time.sleep(1)
-			extendStr = bc.BC + ' Extend to Branches[' + bc.GC + 'y' + bc.BC + '/' + bc.GC + 'n' + bc.BC +']: '
-			extend = str(input(extendStr + bc.GC))
-			if(extend == 'y'):
-				os.system('clear')
-				print(banner)
-				print(eBan + ' [' +bc.GC + ' * * * ' + bc.BC + '] EXTENDER COMING SOON! [' + bc.GC + ' * * * ' + bc.BC + ']\n')
-				time.sleep(1)
-				glassFrog()
-			else:
-				os.system('clear')
-				print(banner)
-				print(bc.BC + ' Returning to Glass Frog...\n')
-				time.sleep(1)
-				glassFrog()
-
-	except KeyboardInterrupt:
-		os.system('clear')
-		print(banner)
-		quit()
+				self.BaseURL = f"{self.BaseURL}/"
 			
-if __name__ == '__main__':
-	glassFrog()
+			if(self.Validator.HasProtocol(self.BaseURL)):
+				return self.BaseURL
+			else:
+				self.Cmd.Clear()
+				print(f"{sd.eBan} Base URL must start with {bc.GC}http://{bc.BC} or {bc.GC}https://{bc.BC}\n")
+				Initiate()
+		else:
+			self.Cmd.Clear()
+			print(f"{sd.eBan} Base URL value cannot be empty\n")
+			self.SetBaseURL()
+
+	def SetKeyword(self):
+		self.Keyword: str = str(input(f"{bc.BC} Keyword: {bc.GC}"))
+
+		if(self.Validator.NotEmpty(self.Keyword)):
+			return self.Keyword
+		else:
+			self.Cmd.Clear()
+			print(f"{sd.eBan} Keyword value cannot be empty\n")
+			self.SetKeyword()
+
+	def SetExtend(self):
+		self.Extend: str = str(input(f"\n{bc.BC} Extend to Branches[{bc.GC}y{bc.BC}/{bc.GC}n{bc.BC}]: {bc.GC}")).lower()
+
+		self.Cmd.Clear()
+		if(self.Validator.NotEmpty(self.Extend)):
+			if(self.Extend == "y"):
+				return "y"
+			else:
+				return "n"
+		else:
+			return "n"
+
+	def StartSearch(self, BaseURL: str, Keyword: str):
+		self.BaseURL: str = BaseURL
+		self.Keyword: str = Keyword
+
+		self.Cmd.Clear()
+
+		self.Branches, self.BranchSetKey = self.Collector.GetBranches(self.BaseURL, self.Keyword)
+		if(self.Branches != None):
+			return self.Branches, self.BranchSetKey
+		else:
+			return None, None
+
+if(__name__ == "__main__"):
+	def Initiate():
+		try:
+			Frog = GlassFrog()
+
+			BaseURL = Frog.SetBaseURL()
+			Keyword = f" {Frog.SetKeyword()} "
+
+			Branches, BranchSetKey = Frog.StartSearch(BaseURL, Keyword)
+			if(Branches != None and BranchSetKey != None):
+				ContentScanner().SearchData(BaseURL=BaseURL, BranchList=Branches, BranchSetKey=BranchSetKey)
+			else:
+				quit()
+
+			# Remove any duplicated data from DataAnalysis/UNKNOWN_DATA_TYPE.txt
+			Command().RemoveDuplicateFileContent(CoreConfig().UnknownDataAnalysisFilePath)
+
+			ShouldExtend = Frog.SetExtend()
+			if(ShouldExtend == "y"):
+				print(f"{sd.eBan} [{bc.GC} * * * {bc.BC}] EXTENDER COMING SOON! [{bc.GC} * * * {bc.BC}]\n")
+
+				# Frog.StartSearch(Branches)
+			else:
+				print(f"{bc.BC} Returned to {CoreConfig().AppName}...\n")
+
+			Initiate()
+
+		except KeyboardInterrupt:
+			quit()
+
+	Command().Clear()
+	Initiate()
